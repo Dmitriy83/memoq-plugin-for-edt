53cb40867aab1a8328bb16e896e16008eff97232
Dmitry Zhiharev <d.zhiharev83@gmail.com>
2023-07-30 18:56:11 +0300

    Unit test has changed

--- org.memoq-for-edt/src/org/memoq_for_edt/internal/UnloadSourceLangDictionariesTest.java
+++ org.memoq-for-edt/src/org/memoq_for_edt/internal/UnloadSourceLangDictionariesTest.java
@@ -23,33 +23,24 @@ class UnloadSourceLangDictionariesTest
         UnloadSourceLangDictionaries testObject = new UnloadSourceLangDictionaries();
 
         String lineToTest = "Method.CreateAdministrator.Var.ErrorText.NStr.Cannot\\ create\\ a user\\ in\\ the\\ catalog\\..Lines=Cannot create a user in the catalog";
-        lineToTest = testObject.fixKeyInLine(lineToTest);
-
-        assertEquals("Method.CreateAdministrator.Var.ErrorText.NStr.Cannot\\ create\\ a\\ user\\ in\\ the\\ catalog\\..Lines=Cannot create a user in the catalog", lineToTest);
+        assertEquals("Method.CreateAdministrator.Var.ErrorText.NStr.Cannot\\ create\\ a\\ user\\ in\\ the\\ catalog\\..Lines=Cannot create a user in the catalog", testObject.fixKeyInLine(lineToTest));
 
         lineToTest = "Method.CreateAdministrator.Var.ErrorText.NStr.Cannot\\ create\\ user\\..Lines=Cannot create a user";
-        lineToTest = testObject.fixKeyInLine(lineToTest);
-
-        assertEquals("Method.CreateAdministrator.Var.ErrorText.NStr.Cannot\\ create\\ user\\..Lines=Cannot create a user", lineToTest);
+        assertEquals("Method.CreateAdministrator.Var.ErrorText.NStr.Cannot\\ create\\ user\\..Lines=Cannot create a user", testObject.fixKeyInLine(lineToTest));
 
         lineToTest = "Method.CreateAdministrator.Var.ErrorText.NStr.Cannot\\ create\\=\\ user\\..Lines=Cannot create a user";
-        lineToTest = testObject.fixKeyInLine(lineToTest);
-
-        assertEquals("Method.CreateAdministrator.Var.ErrorText.NStr.Cannot\\ create\\=\\ user\\..Lines=Cannot create a user", lineToTest);
+        assertEquals("Method.CreateAdministrator.Var.ErrorText.NStr.Cannot\\ create\\=\\ user\\..Lines=Cannot create a user", testObject.fixKeyInLine(lineToTest));
 
         lineToTest = "Method.CreateAdministrator.Var.ErrorText.NStr.Cannot=";
-        lineToTest = testObject.fixKeyInLine(lineToTest);
-
-        assertEquals("Method.CreateAdministrator.Var.ErrorText.NStr.Cannot=", lineToTest);
+        assertEquals("Method.CreateAdministrator.Var.ErrorText.NStr.Cannot=", testObject.fixKeyInLine(lineToTest));
 
         lineToTest = "Synonym=Users";
-        lineToTest = testObject.fixKeyInLine(lineToTest);
-
-        assertEquals("Synonym=Users", lineToTest);
+        assertEquals("Synonym=Users", testObject.fixKeyInLine(lineToTest));
 
         lineToTest = "Method.ExecuteItemReplacement.Var.ErrorText.NStr.Cannot save\\ register\\ records for\\ recorder\\ \"%1\"\\ to\\ record\\ set\\ \"%2\"\\:\\n%3.Lines=Cannot save register records";
-        lineToTest = testObject.fixKeyInLine(lineToTest);
+        assertEquals("Method.ExecuteItemReplacement.Var.ErrorText.NStr.Cannot\\ save\\ register\\ records\\ for\\ recorder\\ \"%1\"\\ to\\ record\\ set\\ \"%2\"\\:\\n%3.Lines=Cannot save register records", testObject.fixKeyInLine(lineToTest));
 
-        assertEquals("Method.ExecuteItemReplacement.Var.ErrorText.NStr.Cannot\\ save\\ register\\ records\\ for\\ recorder\\ \"%1\"\\ to\\ record\\ set\\ \"%2\"\\:\\n%3.Lines=Cannot save register records", lineToTest);
+        lineToTest = "Attribute.Invalid.ToolTip=The external user does not have access to the application but their data is still stored.\\nInvalid external users are hidden from the user lists.\\n";
+        assertEquals("Attribute.Invalid.ToolTip=The external user does not have access to the application but their data is still stored.\\nInvalid external users are hidden from the user lists.\\n", testObject.fixKeyInLine(lineToTest));
     }
 }
